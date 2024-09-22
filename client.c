497d498b23c8d4e48bd90edfd0710640c6a53f90
DarkWhiteOff <zeezou@MacBook-58-inch.localdomain>
2024-09-22 18:13:45 +0200

    2nd commit

--- client.c
+++ client.c
@@ -1,23 +1,42 @@
 #include "minitalk.h"
 
-void    signal_handler(int  signum)
+int	ft_atoi(const char *str)
 {
-    printf("\nSignal sent\n");
+	int	i;
+	int	nbr;
+	int	sign;
+
+	i = 0;
+	nbr = 0;
+	sign = 1;
+	if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	if (!(str[i] >= '0' && str[i] <= '9'))
+		return (0);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	return (nbr * sign);
 }
 
-void    set_SIGUSR1_action()
+int main(int    argc, char  *argv[])
 {
-    struct sigaction sigusr1_action;
-    sigusr1_action.sa_handler = signal_handler;
-    sigemptyset(&sigusr1_action.sa_mask);
-    sigusr1_action.sa_flags = 0;
-    sigaction(SIGUSR1, &sigusr1_action, NULL);
-}
+    pid_t   serverpid;
+	char	*message;
+	int	msgsize;
 
-int main(void)
-{
-    set_SIGUSR1_action();
-    kill(serverpid, SIGUSR1);
+    serverpid = ft_atoi(argv[1]);
+	message = argv[2];
+	msgsize = ft_strlen(argv[2]);
+	while (msgsize--)
+    	kill(serverpid, SIGUSR1);
     while (1)
         continue ;
     return (0);
--- serveur.c
+++ serveur.c
@@ -1,17 +1,27 @@
 #include "minitalk.h"
 
-void    do_this()
+void    signal_manager(int signal)
 {
-    printf("\nSignal recieved\n");
+    int msgsize;
+
+    msgsize = 0;
+    if (signal == SIGUSR1)
+        msgsize++;
+    ft_printf("%d\n", msgsize);
+
 }
 
 int main(void)
 {
     pid_t serverpid;
+    struct sigaction action_sigusr1;
 
     serverpid = getpid();
-    printf("%d\n", serverpid);
-    signal(SIGUSR1, do_this);
+    ft_printf("%d\n", serverpid);
+    action_sigusr1.sa_handler = signal_manager;
+    sigemptyset(&action_sigusr1.sa_mask);
+    action_sigusr1.sa_flags = 0;
+    sigaction(SIGUSR1, &action_sigusr1, NULL);
     while (1)
         continue ;
     return (0);
